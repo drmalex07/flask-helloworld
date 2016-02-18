4746dea0f91e59b00cc3f12a41a27d31dd66afff
Michail Alexakis <alexakis@imis.athena-innovation.gr>
2016-02-18 18:38:43 +0200

    Tie Beaker sessions with Flask's request-wide session variable

--- helloworld/app.py
+++ helloworld/app.py
@@ -5,7 +5,8 @@ import flask
 import urllib
 import logging
 from urllib import urlencode
-from flask import url_for, request, make_response, redirect, abort
+from flask import request, session
+from flask import url_for, make_response, redirect, abort
 from flask import render_template
 import sqlalchemy
 
@@ -14,8 +15,10 @@ from helloworld import model
 def make_app(global_config, **app_config):
 
     app = flask.Flask(__name__)
+    
+    app.config.update(app_config)
 
-    log = logging.getLogger(__name__)
+    log1 = logging.getLogger(__name__)
 
     # Create database session factory for requests
     
@@ -23,14 +26,18 @@ def make_app(global_config, **app_config):
         app_config.get('database.url', 'sqlite://'))
     model.Session.configure(bind=db_engine)
 
+    # Setup template context
+
+    @app.context_processor
+    def setup_template_variables():
+        return dict(foo=session.get('foo'), baz='99')
+    
     # Setup application routes
 
-    @app.route('/')
-    def index():
-        session = request.environ['beaker.session']
-        if not session.has_key('foo'):
-            session['foo'] = 'bar' 
-            session.save()   
+    @app.route('/foo')
+    def remember_foo():
+        if not 'foo' in session:
+            session['foo'] = 'bar'
             return 'foo=' + session['foo'] + " [saved]"
         else:
             return 'foo=' + session['foo']
@@ -49,24 +56,31 @@ def make_app(global_config, **app_config):
         return render_template('article.html', article=article)
    
     @app.route('/new-article', methods=['POST'])
-    def new_article():
-        db_session = model.Session()
-        article = model.Article(
-            title=request.form['title'], body=request.form['body'])
+    def save_new_article():
         redirect_url = None 
-        try:
-            db_session.add(article)
-            db_session.commit()
+        if 'cancel' in request.form:
             redirect_url = url_for('list_articles')
-        except:
-            db_session.abort()
-        finally:
-            db_session.close()
+        else:
+            db_session = model.Session()
+            article = model.Article(
+                title=request.form['title'], body=request.form['body'])
+            try:
+                db_session.add(article)
+                db_session.commit()
+                redirect_url = url_for('list_articles')
+            except:
+                db_session.abort()
+            finally:
+                db_session.close()
         if redirect_url:
             return redirect(redirect_url)
         else:
             abort(500)
   
+    @app.route('/new-article', methods=['GET'])
+    def show_new_article():
+        return render_template('new-article.html')
+    
     @app.route('/environ')
     def print_environ():
         environ_dump = json.dumps({
@@ -76,10 +90,11 @@ def make_app(global_config, **app_config):
         resp.headers['content-type'] = 'application/json; charset=utf-8'
         return resp
 
+    @app.route('/')
     @app.route('/hello')
     @app.route('/hello/<name>')
     def hello(name='nobody'):
-        log.info('Rendering template hello.html')
+        log1.info('Rendering template hello.html')
         return render_template('hello.html', name=name)
 
     return app
